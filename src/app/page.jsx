66f66dedc6e673ffe4000fec8957ab117c6a9edd
HENRY ODUNSI <n.rytech15@gmail.com>
2025-10-30 14:36:24 +0100

    refact

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,6 @@ import Nav from "@/components/layout/Nav";
 import FoodWaste from "@/components/ui/FoodWaste";
 import FoodWasteMatter from "@/components/ui/FoodWasteMatter";
 import Help from "@/components/ui/Help";
-import Stats from "@/components/ui/Stats";
 
 export default function Home() {
   return (
--- src/libs/utils/cloudinary.js
+++ src/libs/utils/cloudinary.js
@@ -1,7 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import { Readable } from "stream";
 
-// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
